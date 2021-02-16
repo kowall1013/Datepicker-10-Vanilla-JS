230fdb92d441250365fd96e3488025a18ca22a12
Mateusz Kowalski <mkmotywacja@gmail.com>
2021-02-16 06:46:31 +0100

    feat: adding date through javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
-// Start writing JavaScript here!
+const yearMonthIndicator = document.querySelector('.datepicker__monthIndicator')
+
+const date = new Date(2021, 1, 16)
+
+const monthsInAYear = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
+
+const month = date.getMonth()
+const monthName = monthsInAYear[month]
+const year = date.getFullYear()
+const lastDayInMonth = new Date(year, month + 1, 0)
+const daysInMonth = lastDayInMonth.getDate()
+
+console.log(daysInMonth);
+
+
+const dateTimeMonth = (month + 1).toString().padStart(2,'0')
+yearMonthIndicator.innerHTML = `<time datetime="${year}-${dateTimeMonth}"> ${monthName} ${year} </time>`
+
+
+
+
+
+
+
+
+
+
+
+
+
