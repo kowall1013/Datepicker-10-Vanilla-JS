bf7307cf8f4bd0f305528e3c87e773961be43903
Mateusz Kowalski <mkmotywacja@gmail.com>
2021-02-17 16:41:50 +0100

    feat: Add functionality gird calendar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const yearMonthIndicator = document.querySelector('.datepicker__monthIndicator')
+const dateGrid = document.querySelector('.datepicker__date-grid')
 
-const date = new Date(2021, 1, 16)
+const date = new Date(2021, 1, 17)
 
 const monthsInAYear = [
   'January',
@@ -23,11 +24,23 @@ const year = date.getFullYear()
 const lastDayInMonth = new Date(year, month + 1, 0)
 const daysInMonth = lastDayInMonth.getDate()
 
-console.log(daysInMonth);
-
 
 const dateTimeMonth = (month + 1).toString().padStart(2,'0')
 yearMonthIndicator.innerHTML = `<time datetime="${year}-${dateTimeMonth}"> ${monthName} ${year} </time>`
+
+for (let day = 1; day <= daysInMonth; day++){
+  const button = document.createElement('button')
+  const dateTimeDay = day.toString().padStart(2, '0')
+  const firstDayOfMonth = date.getDay()
+
+  if (day === 1) button.style.setProperty('--firstDayOfMonth', firstDayOfMonth + 1 )
+
+  button.innerHTML = `
+  <time datetime="${year}-${dateTimeMonth}-${dateTimeDay}">${day}</time>
+  `
+
+  dateGrid.appendChild(button)
+}
 
 
 
