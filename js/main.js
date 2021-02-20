a1fdfec3a96f1e621a4235c53e30515122c588b8
Mateusz Kowalski <mkmotywacja@gmail.com>
2021-02-20 07:42:09 +0100

    feat: positiong calendar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const date = new Date(2021, 1, 18)
 const form = document.querySelector('form')
+const input = form.querySelector('input')
 
  const monthsInAYear = [
    'January',
@@ -26,6 +27,7 @@ const createDateGridHTML = date => {
 
   for (let day = 1; day <= daysInMonth; day++) {
     const button = document.createElement('button')
+    button.setAttribute('type','button')
     if (day === 1) button.style.setProperty('--firstDayOfMonth', firstDayOfTheMonth + 1)
 
     const datetimeDay = day.toString().padStart(2, '0')
@@ -39,12 +41,16 @@ const createDateGridHTML = date => {
 }
 
 const datetimeToDate = dateTime => {
-  const [year, month] = dateTime.split("-").map(num => parseInt(num))
+  const [year, month, day = 1] = dateTime.split("-").map(num => parseInt(num))
 
-  return new Date(year, month - 1)
+  return new Date(year, month - 1, day)
 }
 
-const createDatepicker = date => {
+const toTwoDigitsString = date => {
+  return  date.toString().padStart(2, '0')
+}
+
+const createDatepicker = (date, input) => {
   const datepicker = document.createElement('div')
   datepicker.classList.add('datepicker')
 
@@ -55,13 +61,13 @@ const createDatepicker = date => {
 
   const previousNextMonthButtons = `
     <div class="datepicker__buttons">
-      <button class="datepicker__previous">
+      <button type="button" class="datepicker__previous">
         <svg viewBox="0 0 20 20">
           <path fill="currentColor" d="M7.05 9.293L6.343 10 12 15.657l1.414-1.414L9.172 10l4.242-4.243L12 4.343z" /></svg>
         </svg>
       </button>
 
-      <button class="datepicker__next">
+      <button type="button" class="datepicker__next">
         <svg viewBox="0 0 20 20">
           <path fill="currentColor" d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
         </svg>
@@ -87,11 +93,19 @@ const createDatepicker = date => {
       </div>
     </div>
   `
-
   datepicker.innerHTML = `
     ${previousNextMonthButtons}
     ${calendar}
   `
+
+  const inputRect = input.getBoundingClientRect()
+  const oneRem = parseFloat(getComputedStyle(document.body)['font-size'])
+  datepicker.style.left = `${inputRect.left}px`
+  datepicker.style.top = `${inputRect.bottom + oneRem}px`
+
+  
+  
+
 
   const getDateFromYearMonthIndicator = _ => {
     const time = datepicker.querySelector('.datepicker__monthIndicator').firstElementChild
@@ -130,10 +144,33 @@ const createDatepicker = date => {
 
   })
 
+  const dategrid = datepicker.querySelector('.datepicker__date-grid')
+  dategrid.addEventListener('click', event => {
+    if (!event.target.matches('button')) return
+    const button = event.target
+    const buttons = [...button.parentElement.children]
+
+    buttons.forEach(btn => btn.classList.remove('is-selected'))
+    button.classList.add('is-selected')
+
+    const time = button.firstElementChild
+    const datetime = time.getAttribute('datetime')
+    const selectedDate = datetimeToDate(datetime)
+
+    const year = selectedDate.getFullYear()
+    const month = toTwoDigitsString(selectedDate.getMonth() + 1)
+    const day = toTwoDigitsString(selectedDate.getDate())
+
+    const formatted = `${day}/${month}/${year}`
+    input.value = formatted
+
+  })
+
   return datepicker
+
 }
 
-form.appendChild(createDatepicker(date))
+document.body.appendChild(createDatepicker(date, input))
 
 
 
